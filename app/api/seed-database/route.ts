ad9250b51999ccffb55e4bfddf6a01b7497eaa56
John Darlington <onuohadarlington38@gmail.com>
2026-05-19 08:43:25 +0100

    made test

--- app/api/seed-database/route.ts
+++ app/api/seed-database/route.ts
@@ -1,24 +1,24 @@
-import { NextResponse } from 'next/server';
-import mongoose from 'mongoose';
-import ProductModel from '~/lib/models/rooms';
-import { houseHoldMaterials } from '~/app/dummy-data';
-const MONGODB_URI = process.env.MONGODB_URI as string;
-export async function POST() {
-   try {
-      if (mongoose.connection.readyState === 0) {
-         await mongoose.connect(MONGODB_URI, {
-            //@ts-expect-error: cause why not
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-         });
-      }
+// import { NextResponse } from 'next/server';
+// import mongoose from 'mongoose';
+// import ProductModel from '~/lib/models/rooms';
+// import { houseHoldMaterials } from '~/app/dummy-data';
+// const MONGODB_URI = process.env.MONGODB_URI as string;
+// export async function POST() {
+//    try {
+//       if (mongoose.connection.readyState === 0) {
+//          await mongoose.connect(MONGODB_URI, {
+//             //@ts-expect-error: cause why not
+//             useNewUrlParser: true,
+//             useUnifiedTopology: true,
+//          });
+//       }
 
-      await ProductModel.insertMany(houseHoldMaterials);
-      return NextResponse.json({ message: 'Database seeded successfully!' });
-   } catch (error: any) {
-      return NextResponse.json(
-         { message: 'Error seeding database', error: error.message },
-         { status: 500 }
-      );
-   }
-}
+//       await ProductModel.insertMany(houseHoldMaterials);
+//       return NextResponse.json({ message: 'Database seeded successfully!' });
+//    } catch (error: any) {
+//       return NextResponse.json(
+//          { message: 'Error seeding database', error: error.message },
+//          { status: 500 }
+//       );
+//    }
+// }
